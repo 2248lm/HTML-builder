a83324c6a13175b1378aec9724512761a5c36630
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-05-19 23:07:26 +0600

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const fs = require('fs');
+const { stdin } = process;
+const fileDirectory = path.join(__dirname, 'note.txt');
+const writeStream = fs.createWriteStream(path.join(fileDirectory));
+console.log('\n1. Пожалуйста, введите своё сообщение и нажмите Enter.');
+console.log('2. Если Вы хотите выйти из потока записи, нажмите сочетание клавиш Ctrl + C или введите exit и нажмите Enter.\n');
+stdin.on('data', data => {
+  const message = data.toString().trim();
+  if (message === 'exit') {
+    writeStream.end();
+    process.on('exit', () => console.log('\nУдачи в изучении Node.js!\n'));
+    process.exit();
+  }
+  else {
+    writeStream.write(data);
+  }
+});
+process.on('SIGINT', () => {
+  console.log('\nУдачи в изучении Node.js!\n');
+  process.exit();
+});
