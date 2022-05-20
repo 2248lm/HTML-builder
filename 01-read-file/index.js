8a2385d491a981be4e87bb8c3b13e6f3c1e223d2
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-05-21 00:38:36 +0600

    refactor: change way of readable stream (in chunks)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,4 +2,6 @@ const path = require('path');
 const fs = require('fs');
 const fileDirectory = path.join(__dirname, 'text.txt');
 const fileInnerText = fs.createReadStream(fileDirectory, 'utf-8');
-fileInnerText.on('data', data => console.log(data));
+let data = '';
+fileInnerText.on('data', chunk => data += chunk);
+fileInnerText.on('end', () => console.log(data));
