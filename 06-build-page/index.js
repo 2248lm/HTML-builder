8d428ccad95606b77dd7ff1f369288017e44cb6c
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-05-22 04:56:19 +0600

    feat: add components connection

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -9,22 +9,56 @@ const assets = path.join(projectDist, 'assets');
 const template = path.join(__dirname, 'template.html');
 const stylesSource = path.join(__dirname, 'styles');
 const assetsSource = path.join(__dirname, 'assets');
-const readTemplate = fs.createReadStream(template, 'utf-8');
-
-const writeIndexHtml = fs.createWriteStream(indexHtml);
-const writeStyles = fs.createWriteStream(path.join(styleCSS));
+const componentsFolder = path.join(__dirname, 'components');
 
 fs.mkdir(projectDist, { recursive: true }, err => {
   if (err) { throw err; }
 });
 
-readTemplate.on('data', data => {
-  data = data.replace(/\{\{/g, '<');
-  data = data.replace(/\}\}/g, '>');
-  writeIndexHtml.write(data)
+const readTemplate = fs.createReadStream(template, 'utf-8');
+let templateInner = '';
+readTemplate.on('data', chunk => {
+  templateInner += chunk;
+});
+readTemplate.on('end', () => {
+  findTag(templateInner);
 });
 readTemplate.on('error', error => console.log('Error: ', error.message));
+const findTag = (text) => {
+  fs.readdir(componentsFolder, { withFileTypes: true }, (err, files) => {
+    if (err) { throw err; }
+    const tags = text.match(/{{(.*?)}}/g);
+    tags.forEach(tagItem => {
+      const tag = tagItem.split('').filter(e => !/[\{\}]/g.test(e)).join('').trim();
+      files.forEach(file => {
+        if (file.isFile()) {
+          const fileWay = path.join(componentsFolder, file.name);
+          fs.stat(fileWay, (err, stats) => {
+            const fileObject = path.parse(path.resolve(file.name));
+            if (err) { throw err; }
+            else {
+              if (fileObject.ext === '.html' && fileObject.name === tag) {
+                let componentInner = '';
+                const readFile = fs.createReadStream(fileWay, 'utf-8');
+                readFile.on('data', chunk => {
+                  componentInner += chunk;
+                });
+                readFile.on('end', () => {
+                  text = text.replace(tagItem, "\n" + componentInner + "\n");
+                  const writeIndexHtml = fs.createWriteStream(indexHtml);
+                  writeIndexHtml.write(text);
+                });
+                readFile.on('error', error => console.log('Error: ', error.message));
+              }
+            }
+          });
+        }
+      });
+    });
+  });
+}
 
+const writeStyles = fs.createWriteStream(path.join(styleCSS));
 fs.readdir(stylesSource, { withFileTypes: true }, (err, files) => {
   if (err) { throw err; }
   else {
